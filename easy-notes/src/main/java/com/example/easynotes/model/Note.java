3b85897b2c292d7aab329563086e86ea0845ef74
Gixx17 <luigipetti01@gmail.com>
2023-05-25 15:00:09 +0200

    Aggiunto get e set

--- easy-notes/src/main/java/com/example/easynotes/model/Note.java
+++ easy-notes/src/main/java/com/example/easynotes/model/Note.java
@@ -17,7 +17,6 @@ import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name="notes")
-
 public class Note implements Serializable{
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
@@ -33,6 +32,41 @@ public class Note implements Serializable{
 	@Temporal(TemporalType.TIMESTAMP)
 	@CreatedDate
 	private Date createdAt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	
+	
 }
 
 
